b6bed18be8f08e3f5b98fde54d8ff1a8804ca082
Elizabeth Gorbunova <egorbunova016@gmail.com>
2024-07-10 20:42:56 +0200

    configured settings provided for authentication and authorization

--- OutOfOfficeApp/OutOfOfficeApp/Program.cs
+++ OutOfOfficeApp/OutOfOfficeApp/Program.cs
@@ -5,12 +5,24 @@ using OutOfOfficeApp.Entities;
 using OutOfOfficeApp.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using FluentValidation;
+using OutOfOfficeApp.Models;
+using OutOfOfficeApp.Models.Validators;
+using FluentValidation.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using OutOfOfficeApp.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<OOODbContext>();
@@ -19,9 +31,33 @@ builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IApprovalRequestService, ApprovalRequestService>();
 builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<RegisterUserDtoIn>, RegisterUserDtoInValidator>();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 
+var authenticationSettings = new AuthenticationSettings();
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+builder.Services.AddSingleton(authenticationSettings);
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -37,24 +73,21 @@ var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
 seeder.Seed();
 
-
-
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseAuthentication();
 /*app.UseHttpsRedirection();*/
 
 app.UseSwagger();
 app.MapSwagger();
 app.UseSwaggerUI();
 
-/*app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OutOfOfficeApp");
-});*/
+
 
 app.UseStaticFiles();
 
 app.UseRouting();
 
-/*app.UseAuthorization();*/
+app.UseAuthorization();
 app.MapControllers();
 
 app.MapRazorPages();
