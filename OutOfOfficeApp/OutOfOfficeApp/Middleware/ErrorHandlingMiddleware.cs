9dea687e4d112845d88be4be8ec7486965c85b7e
Elizabeth Gorbunova <egorbunova016@gmail.com>
2024-07-10 20:35:32 +0200

    Error handling middleware added

---
+++ OutOfOfficeApp/OutOfOfficeApp/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using OutOfOfficeApp.Exceptions;
+
+namespace OutOfOfficeApp.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequest)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequest.Message);
+            }
+            catch (Exception ex)
+            {
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
